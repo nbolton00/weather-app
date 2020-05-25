90a04417a2df27e2b3620319c2ae3583be737f4b
Nicola Bolton <nicola.bolton@yorkandchapel.com>
2020-05-24 23:19:05 -0700

    implementing the weather api

--- src/App.js
+++ src/App.js
@@ -1,30 +1,60 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import { TodaysWeather } from './components/TodaysWeather';
 import { DailyWeather } from './components/DailyWeather';
+
 
 //4fef9137ac3a7273bccae0afcc6a2f01
 //lat 49.278997,
 // lng  -123.119100
 
-//https://api.openweathermap.org/data/2.5/onecall?lat=49.278997&lon=-123.119100&appid=4fef9137ac3a7273bccae0afcc6a2f01
+//
 
 function App() {
+  const [dailyWeather, setDailyWeather] = useState([]);
+  const [currentWeather, setCurrentWeather] = useState({});
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch('https://api.openweathermap.org/data/2.5/onecall?lat=49.278997&lon=-123.119100&exclude=hourly&units=metric&appid=4fef9137ac3a7273bccae0afcc6a2f01')
+        .then(response => response.json())
+        .then(json => {
+          console.log(json)
+          setDailyWeather(json.daily.slice(1));
+          setCurrentWeather(json.current);
+        });
+    };
+    fetchData();
+  }, []);
+
+  const getDay = (timestamp) =>{
+    const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']; 
+    const date = new Date(timestamp * 1000);
+    return days[date.getDay()];
+  }
+
   return (
     <div className="App">
       <div className="App-header">
         <TodaysWeather icon="10n" description="Well hot" temp="24°" feelslike="Feels like 28°"></TodaysWeather>
         
-        <div className="App-container">
+        <div className="dailyContainer">
             <h2>Next 7 days...</h2>
-            <div className ="Weather-daily">
-              <DailyWeather day="Mon" icon="10n" temp="19°"></DailyWeather>
+            <div className ="dailyWeather">
+
+              {dailyWeather.map((day, index) => {
+                return(
+                  <DailyWeather key={`day-${index}`} day={getDay(day.dt)} icon={day.weather[0]['icon']} temp={Math.round(day.temp.max)}></DailyWeather>
+                )
+              })}
+
+              {/* <DailyWeather day="Mon" icon="10n" temp="19°"></DailyWeather>
               <DailyWeather day="Tue" icon="10n" temp="19°"></DailyWeather>
               <DailyWeather day="Wed" icon="10n" temp="19°"></DailyWeather>
               <DailyWeather day="Thu" icon="10n" temp="19°"></DailyWeather>
               <DailyWeather day="Fri" icon="10n" temp="19°"></DailyWeather>
               <DailyWeather day="Sat" icon="10n" temp="19°"></DailyWeather>
-              <DailyWeather day="Sun" icon="10n" temp="19°"></DailyWeather>
+              <DailyWeather day="Sun" icon="10n" temp="19°"></DailyWeather> */}
             </div>
         </div>
       </div>
--- src/components/DailyWeather.js
+++ src/components/DailyWeather.js
@@ -1,38 +1,38 @@
 import React from 'react';
-import styled from "styled-components";
+// import styled from "styled-components";
 // import { defaultTheme } from "../utils";
 // import { typeScale } from "../utils";
 
 
-const WeatherContainer = styled.div`
-    display: flex; 
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    border-right: 1px dotted #dfe3e8;
-    padding: 0 1.2rem;
-    &:last-child {
-        border-right: none;
-    }
-`;
+// const WeatherContainer = styled.div`
+//     display: flex; 
+//     flex-direction: column;
+//     justify-content: flex-start;
+//     align-items: center;
+//     border-right: 1px dotted #dfe3e8;
+//     padding: 0 1.2rem;
+//     &:last-child {
+//         border-right: none;
+//     }
+// `;
 
-const DailyHeader = styled.div`
-    text-transform: uppercase;
-`;
-const DailyIcon = styled.img`
-    margin:.75rem 0;
-`;
-const DailyTemp = styled.div`
-`;
+// const DailyHeader = styled.div`
+//     text-transform: uppercase;
+// `;
+// const DailyIcon = styled.img`
+//     margin:.75rem 0;
+// `;
+// const DailyTemp = styled.div`
+// `;
 
 
 export const DailyWeather = (props) => {
     const iconImageUrl = `../../${props.icon}.svg`;
     return (
-        <WeatherContainer>
-            <DailyHeader>{props.day}</DailyHeader>
-            <DailyIcon src={iconImageUrl} alt={props.summary} />
-            <DailyTemp>{props.temp}</DailyTemp>
-        </WeatherContainer>
+        <div className="dailyItem">
+            <p>{props.day}</p>
+            <img src={iconImageUrl} alt={props.summary} />
+            <p>{props.temp}</p>
+        </div>
     )
 }
